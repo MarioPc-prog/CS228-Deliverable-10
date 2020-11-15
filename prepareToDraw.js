9b9839d64bc7fab91981a3d2350bebdbfeb590a0
Mario <mavega@uvm.edu>
2020-11-15 12:02:20 -0500

    Adding files from del 09

---
+++ prepareToDraw.js
@@ -0,0 +1,24 @@
+var img;
+var arrowLeft;
+var arrowRight;
+var arrowUp;
+var arrowDown;
+var arrowTowards;
+var arrowAway;
+var ASL1;
+var ASL2;
+function setup() {
+createCanvas(window.innerWidth,window.innerHeight);
+img = loadImage('https://i.imgur.com/U0ZM0Jb.jpg');
+arrowLeft = loadImage('https://i.imgur.com/XLenHyD.jpg');
+arrowRight = loadImage('https://i.imgur.com/PiQZ0Y9.jpg');
+arrowUp = loadImage('https://i.imgur.com/0XpTg8M.jpg');
+arrowDown = loadImage('https://i.imgur.com/C2gYnRP.jpg');
+arrowTowards = loadImage('https://i.imgur.com/PLZ5h2a.jpg');
+arrowAway = loadImage('https://i.imgur.com/mGIj1ZE.jpg');
+ASL1 = loadImage("https://i.imgur.com/bGZRSdW.jpg");
+ASL2 = loadImage("https://i.imgur.com/zQ4DOIe.jpg");
+
+
+
+}
